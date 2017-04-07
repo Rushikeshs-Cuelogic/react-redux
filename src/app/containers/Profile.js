bf4a2981d1742a9df01845098b67a428ce4ebf32
Rushikesh <rushikesh.shelar@cuelogic.com>
2017-04-07 17:47:24 +0530

    change html

--- src/app/containers/Profile.js
+++ src/app/containers/Profile.js
@@ -9,29 +9,29 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="container">
-            <div className="col-xs-10 col-xs-offset-1" >
-                <nav className="navbar navbar-inverse">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand" href="http://www.cuelogic.com/">Cuelogic</a>
+                <div className="col-xs-10 col-xs-offset-1" >
+                    <nav className="navbar navbar-inverse">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="http://www.cuelogic.com/">Cuelogic</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="myNavbar">
+                            <ul className="nav navbar-nav">
+                                <li><Link to={"/"} activeClassName={"active"}>Logout</Link> </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div className="row" className="col-xs-10 col-xs-offset-1" >
+                        <label>User Id:{this.props.user.user.username}</label>
+                        <hr />
+                        <label>Email:{this.props.user.user.email}</label>
+                        <hr />
                     </div>
-                    <div className="collapse navbar-collapse" id="myNavbar">
-                        <ul className="nav navbar-nav">
-                            <li><Link to={"/"} activeClassName={"active"}>Logout</Link> </li>
-                        </ul>
-                    </div>
-                </nav>
-                <div className="row" className="col-xs-10 col-xs-offset-1" >
-                    <label>User Id:{this.props.user.user.username}</label>
-                    <hr />
-                    <label>Email:{this.props.user.user.email}</label>
-                    <hr />
                 </div>
             </div>
-              </div>
 
         );
     }
--- src/app/store.js
+++ src/app/store.js
@@ -3,9 +3,9 @@ import AllReducers from "./reducers";
 import { applyMiddleware } from "redux";
 import logger from "redux-logger";
 const store = createStore(
-  AllReducers,{}, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger)
+  AllReducers, {}, applyMiddleware(logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 );
 export default store;
 
